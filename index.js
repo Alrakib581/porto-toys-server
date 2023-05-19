17f24fa670e6b1b0de09a26ee0ed1bbf23ef3076
alrakib581 <alrakib581@gmail.com>
2023-05-19 21:22:20 +0600

    vercel error solve

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
+const corsConfig = {
+    origin: '',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig));
+
 app.use(express.json());
 
 
@@ -21,9 +29,9 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+    await client.connect();
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
     //photo gallery collection code.
 
     const photoCollection = client.db('portoToys').collection('gallery');
