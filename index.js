63821e800087586f27191feb16da5f5cd02640b4
alrakib581 <alrakib581@gmail.com>
2023-05-19 23:43:14 +0600

    cors error and vercel error solve

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.use(cors());
+app.use(cors());
 const corsConfig = {
     origin: '',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
 app.use(cors(corsConfig))
-app.options("", cors(corsConfig));
+app.options("", cors(corsConfig))
 
 app.use(express.json());
 
